8f58589cf3bb66d72d5669000acd389b8bfc8fb4
Diego Medeiros <diegoo.fm@gmail.com>
2015-06-26 00:05:48 -0300

    adding portfolio entries

--- assets/javascripts/application.js
+++ assets/javascripts/application.js
@@ -43,4 +43,86 @@
   });
   $(".accordion-content").addClass("defualt-hidden");
 
+  // Portfolio Lightbox Popup Elements
+  $(".cbox-gallary1").colorbox({
+    rel: 'gallary',
+    maxWidth: "95%",
+    maxHeight: "95%",
+    title: function(){
+      var url = $(this).data('link');
+      var title = $(this).attr('title');
+
+      if (url) {
+        return '<a href="' + url + '" target="_blank">' + title + ' (Ver online)</a>';
+      }  else {
+        return title;
+      }
+    }
+  });
+
+  containerGridMasonry();
+
 })(jQuery);
+
+
+
+function containerGridMasonry() {
+
+    // Gria Element
+    if ($(this).length > 0) {
+        // ISOTOPE MASONRY ELEMENT  ||--------------
+        var $container = $('.container-masonry');
+        $container.imagesLoaded(function () {
+            $container.isotope({
+                itemSelector: '.nf-item',
+                layoutMode: 'masonry',
+                masonry: {
+                    columnWidth: 0,
+                    gutter: 0
+                },
+            });
+        });
+
+        // bind filter button click
+        $('.container-filter').on('click', '.categories', function () {
+            var filterValue = $(this).attr('data-filter');
+            $container.isotope({ filter: filterValue });
+        });
+
+        // ISOTOPE GRID ELEMENT  ||--------------
+        var $container2 = $('.container-grid');
+        $container2.imagesLoaded(function () {
+            $container2.isotope({
+                itemSelector: '.nf-item',
+                layoutMode: 'fitRows'
+            });
+        });
+
+        // bind filter categories click
+        $('.container-filter').on('click', '.categories', function () {
+            var filterValue = $(this).attr('data-filter');
+            $container2.isotope({ filter: filterValue });
+        });
+
+        // change active class on categories
+        $('.categories-filter').each(function (i, buttonGroup) {
+            var $buttonGroup = $(buttonGroup);
+            $buttonGroup.on('click', '.categories', function () {
+                $buttonGroup.find('.active').removeClass('active');
+                $(this).addClass('active');
+            });
+
+        });
+    };
+
+    // Masonry Element
+    if ($(this).length > 0) {
+        var container = $('.masonry');
+        container.masonry({
+            // columnWidth: 0,
+            itemSelector: '.nf-item'
+        });
+    };
+
+
+};
